fae77ed7ab4f464e2a319b978782651599726069
GBogonis <george.bogonis@student39.org>
2022-09-07 11:08:18 -0400

    finish first calculator

--- donutCalculator.py
+++ donutCalculator.py
@@ -1,26 +1,36 @@
 #var set-up
 costOfDozen = 4.99
 costOfSingle = costOfDozen/12
+costOfCoffee = 2.99
+cost = 0
 
-#calculating how many donuts the user wants and how much it will cost
+#calculating how many donuts and coffees the user wants and how much it will cost
 
-prompt = 'How many donuts do you want?\n'
+prompt = 'How many donuts would you like?\n'
 donutsWanted = input(prompt)
 
 if(int(donutsWanted) > 12):
     print("only 12 in stock, sorry")
 else:
-    cost = round(int(donutsWanted)*costOfSingle,2)
-    print("that will cost", cost, "$")
+    cost = cost + round(int(donutsWanted)*costOfSingle,2)
+    #print("that will cost", cost, "$")
+
+prompt2 = 'and would you like any coffee?\n'
+coffeeWanted = input(prompt2)
+
+cost = cost + round(int(coffeeWanted)*costOfCoffee,2)
+print("Ok, Your total cost is", cost,"$")
 
 #calculating how much money is owed compaired to how much money is given
 
-prompt2 = 'How much can you pay?'
-amountPaid = input(prompt2)
+prompt3 = 'How much can you pay?\n'
+amountPaid = input(prompt3)
 
 if(int(amountPaid) < cost):
-    print("You are short", abs(int(amountPaid)-cost), "$")
+    print("You are short", round(abs(int(amountPaid)-cost),2), "$")
 elif(int(amountPaid) > cost):
-    print("You over paid", int(amountPaid)-cost,"$")
+    print("You over paid", round(int(amountPaid)-cost,2),"$")
 else:
-    print("You the exact amount needed!")
+    print("You paid the exact amount needed!")
+
+print("Thank You")
