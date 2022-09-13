e3786bac554f8b74cb65751b0efd1a2b436c3fad
GBogonis <george.bogonis@student39.org>
2022-09-13 10:39:25 -0400

    finish paycheck calculator

--- paycheckCalculator.py
+++ paycheckCalculator.py
@@ -3,24 +3,29 @@
 #gathering info from user
 name = input('Hello enployee! What is your name?\n')
 print("Ok",name,"this program is designed to help you figure out what you paycheck should be!")
-hourlyRate = round(input('so, how much money do you make per hour?\n'),2)
+hourlyRate = input('so, how much money do you make per hour?\n')
 hoursWorked = input('and how many hours did you work\n')
-print("ok",name,"you worked",hoursWorked,"for",hourlyRate,"per hour")
+print("ok",name,"you worked",float(hoursWorked),"for",round(float(hourlyRate),2),"$ per hour")
 correct = input('correct? (yes or no)\n')
 
 #asking if the users info is correct
 if(correct == "yes"):
     print("ok great!")
     #asking the user if they want to evade taxes is always a good thing to do
-    tax = input('so would you like to evade taxes today?\n')
+    tax = input('so would you like to evade taxes today? (yea or no)\n')
     cont = True
 elif(correct == "no"):
     print("I recomend restarting then")
+    tax = 'no'
     cont = False
 
 
-if(tax == "yes",cont):
-    print("correct choice, your total gross income is",hourlyRate*hoursWorked)
+if(tax == "yes" and cont):
+    print("correct choice, your total gross income is",float(hourlyRate)*float(hoursWorked),"$")
     print("we recomend that you watch out for the IRS")
-elif(tax == "no",cont):
-    print("thats ashame")
+elif(tax == "no" and cont):
+    #calculating taxes 
+    print("thats ashame you could have gotten", float(hourlyRate)*float(hoursWorked),"$")
+    print("your pay after taxes", round((float(hourlyRate)*float(hoursWorked))*0.8,2),"$")
+
+print("Thank you!")
