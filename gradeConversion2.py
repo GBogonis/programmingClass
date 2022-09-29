f48c9d531c3de9a06c77d4e1b5a1767e77c679d4
GBogonis <george.bogonis@student39.org>
2022-09-29 09:32:35 -0400

    update

--- gradeConversion2.py
+++ gradeConversion2.py
@@ -16,19 +16,35 @@ def convertGrades(double):
     except:
         return"function expects a float"
 
-#asking the user for their grade
-gradeInput = input('Hello! to convert your grade first we need to know your grade! \n')
+def askGrade():
+    #asking the user for their grade
+    gradeInput = input('what is the grade you need to convert? \n')
+    #converting the input to a float
+    try:
+        grade = float(gradeInput)
+    except:
+        print("please enter a number next time")
+        exit()
+    return grade
 
-#converting the input to a float
-try:
-    grade = float(gradeInput)
-except:
-    print("please enter a number next time")
+#start main code
+print("Hello and welcome to the grade coversion program!")
+
+#the 'convertGrades' function returns a string and the 'askGrade' asks the user for there grade and coverts it to a float
+print(convertGrades(askGrade()))
+
+#asking if user wants to convert more grades
+cont = input('is there any more grades that you need converted?\n')
+
+if(cont == 'yes' or cont == 'Yes'):
+    print('ok')
+    print(convertGrades(askGrade()))
+elif(cont == 'No' or cont == 'no'):
+    print("ok then :(")
     exit()
-
-print(convertGrades(grade))
-
-
+else:
+    print("program expects a yes or no")
+    exit()
 
 print("Thank you!")
 #End
