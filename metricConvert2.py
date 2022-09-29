39df6b4a8d39d45db4fa8f11e1526b70e023cb65
GBogonis <george.bogonis@student39.org>
2022-09-29 10:51:30 -0400

    update

---
+++ metricConvert2.py
@@ -0,0 +1,43 @@
+#start
+
+#calculation function to convert and return all the types of information based on the parameters
+def calculateData(dataType, data):
+    if(dataType == 1):
+        #calculate temperature
+        return round((data-32)*(5/9),0)
+    if(dataType == 2):
+        #calculate wind speeds
+        return round(data*1.609,2)
+    if(dataType == 3):
+        #calculate snowfall
+        return round(data*2.54,2)
+
+#a function to gather data from the user and print the results 
+def gatherAndShowData(option):
+    if(option == 1):
+        Temp = input('ok, how cold is it right now? (in Fahrenheit)\n')
+        print("Ok, so the temperature in Celsius is", calculateData(1,float(Temp)), "degrees")
+        #exit()
+    elif(option == 2):
+        Miles = input('ok, how fast is the wind going right now? (in miles)\n')
+        print("So the wind speeds in kilometers is about", calculateData(2,float(Miles)), "per hour")
+        #exit()
+    elif(option == 3):
+        Inches = input('ok, how deep was the snow from the recent snow storm? (in inches) \n')
+        print("The amount of snow on the ground in centimeters is", calculateData(3,float(Inches)),"CM")
+        #exit()
+    else:
+        print("program expected you to type 1, 2, or 3")
+
+#start main code
+print("Hello! this program is designed to help convert units of measurement for weather from Fahrenheit to Celsius or imperial to metric!")
+print("First what are you trying to convert? option 1: temperature, option 2: windspeed, option 3: snowfall")
+
+optionInput = input('please type the number of the option you would like to select\n')
+try:
+    selectedOption = float(optionInput)
+except:
+    print("the program expects you to type the number 1, 2, or 3")
+    exit()
+
+gatherAndShowData(float(selectedOption))
