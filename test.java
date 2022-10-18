d630f0f33443b95d77cc591748af86d572d410be
GBogonis <george.bogonis@student39.org>
2022-10-18 09:50:42 -0400

    update

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;  // Import the Scanner class
+
+
 public class test {
 
     public static void main(String[] args) {
         System.out.println("hello wolrd");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter number");
+        int num = myObj.nextInt();  // Read user input
+        System.out.println(10 + num);  // Output user input
     }
 
 }
