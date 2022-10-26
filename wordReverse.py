49454c122c86d7c5caff623dd71975d0b2dd4aa7
GBogonis <95968706+GBogonis@users.noreply.github.com>
2022-10-26 14:29:11 -0400

    reverse program

--- wordReverse.py
+++ wordReverse.py
@@ -1,4 +1,24 @@
 #start
-letterList = []
+
+#function to reverse words
 def reverse(word):
+    #makes the string into a list, reverses the list, and turns it back into a string and returns that
+    letterList = []
     letterList.extend(word)
+    letterList.reverse()
+    return ''.join(letterList)
+
+print("Hello! this program is made to reverse any word you give it")
+
+#loop that lets the users reverse as many words as they want
+while True:
+    word = input("What word would you like to reverse?\n")
+    print(reverse(word))
+    cont = input("whould you like to reverse another word?\n")
+    if(cont == 'yes' or cont == 'Yes'):
+        print("ok")
+    else:
+        print("I'll take that as a no.")
+        break
+    
+print("Thank you!")
