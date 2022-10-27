261cf409f4af8e2fbccef7194a77f881aac934b1
GBogonis <95968706+GBogonis@users.noreply.github.com>
2022-10-27 12:02:01 -0400

    some small changes

--- wordReverse.py
+++ wordReverse.py
@@ -13,10 +13,13 @@ print("Hello! this program is made to reverse any word you give it")
 #loop that lets the users reverse as many words as they want
 while True:
     word = input("What word would you like to reverse?\n")
-    print(reverse(word))
+    print('The word inputted was',word,'that word reversed is:',reverse(word))
     cont = input("whould you like to reverse another word?\n")
     if(cont == 'yes' or cont == 'Yes'):
         print("ok")
+    elif(cont == 'no' or cont == 'No'):
+        print('ok')
+        break
     else:
         print("I'll take that as a no.")
         break
