bdc6398ad4821545c3454cb89bbbb860ecca8abd
GBogonis <george.bogonis@student39.org>
2022-11-17 14:27:41 -0500

    data list

--- peopleLists.py
+++ peopleLists.py
@@ -1,5 +1,33 @@
 #start
 peopleFile = open("peoples.csv")
 
-for item in peopleFile:
-    print(item)
+def merryFestivus():
+    peopleFile.seek(0)
+    for item in peopleFile:
+        item = item.rstrip()
+        itemList = item.split(',')
+        fullName = itemList[1] + ' ' + itemList[0]
+        print("Have a merry Festivus", fullName + "!")
+
+def sortNames():
+    peopleFile.seek(0)
+    listOfLists = []
+    for item in peopleFile:
+        item = item.rstrip()
+        itemList = item.split(',')
+        listOfLists.append(itemList)
+    listOfLists.sort()
+    for stuff in listOfLists:
+        print(stuff[1] + ' ' + stuff[0])
+    
+
+sortNames()
+
+
+
+    
+        
+
+
+
+
