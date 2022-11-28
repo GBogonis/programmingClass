5078e7d6a0a12553a0d3a9c06f4e90ecd0df9000
GBogonis <george.bogonis@student39.org>
2022-11-28 09:05:52 -0500

    update

--- listsHonorsProject.py
+++ listsHonorsProject.py
@@ -1,26 +1,37 @@
 #start
+#open file
 poemFile = open("Cambridgeladies.txt")
 poemList = poemFile.readlines()
+
+#vars for holding the shortest and longest words
 longestWord = ''
 shortestWord = '                                      '
+
+#loop for every line in the poem
 for line in poemList:
+    #sets the length for the words every loop so its always up to date
     longestWordLen = len(longestWord)
     shortestWordLen = len(shortestWord)
+
     line = line.rstrip()
-    if line == "\n":
-        print("yep")
     print(line)
+    #split the line up into a list and sorts it 
     lineList = line.split(' ')
     lineList.sort()
+
+    #loop for every word in the sorted line list
     for word in lineList:
-        #
-        # print(word)
-        # word = word.strip()
+
+        word = word.strip()
+        #compairs the words to the current shortest and longest words saved
         if(len(word) > longestWordLen):
             longestWord = word
         if(len(word) < shortestWordLen and len(word) >= 1):
             shortestWord = word
+    #prints the sorted line list as a string
     print(' '.join(lineList))
+
+#print the longest and shortest words that were found.
 print("the longest word is", longestWord)
-print("the shortest word is", '!'+shortestWord+'!')
-    
+print("the shortest word is", shortestWord)
+#end
