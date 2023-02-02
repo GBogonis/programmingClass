1974e55cbbf306503c8618eb245cfcb528b9a34c
GBogonis <george.bogonis@student39.org>
2023-02-02 09:20:51 -0500

    I am become silly, maker of fun :D

--- pyArt.py
+++ pyArt.py
@@ -1,11 +1,13 @@
+#start
+
+#import is in a try so if the import doesn't work the program will quit before it breaks
 try:
     import pygame
 except:
     print("could not import pygame")
     exit() 
 
-# Define some colors
-BLACK = (0, 0, 0)
+# Define some colors (I looked up the real RGB values for the colors of the flag)
 WHITE = (255, 255, 255)
 BLUE = (0, 33, 71)
 RED = (187,19,62)
@@ -17,11 +19,11 @@ pygame.init()
 size = (1425, 714)
 screen = pygame.display.set_mode(size)
  
-pygame.display.set_caption("Art")
+pygame.display.set_caption("The Flag")
 
-Pos = 360
+#off-set type varible for drawing things
 dif = 686
-count = 0
+
 # Set variable to run loop until the user clicks the close button.
 done = False
  
@@ -39,18 +41,23 @@ while not done:
 
     # --- Drawing code should go here
         
+        #loop for drawing the red stripes
         for dif in range(dif, -5, -110):
-            count += 1
             pygame.draw.line(screen,(RED),(0,0+dif),(1425,0+dif), 54)
-        pygame.draw.rect(screen, (BLUE), pygame.Rect(0, 0, 590, 384))
-            
-        
 
-        #pygame.draw.line(screen,(BLUE),(Pos,250),70, 0)
+        #drawing the blue rectangle
+        pygame.draw.rect(screen, (BLUE), pygame.Rect(0, 0, 590, 384))
+
+        #drawing the white squares because stars are too hard :(
+        for i in range(10, -1, -1):
+            pygame.draw.rect(screen, (WHITE), pygame.Rect(63*i-100,10,30,30))
+            pygame.draw.rect(screen, (WHITE), pygame.Rect(63*i-100,86,30,30))
+            pygame.draw.rect(screen, (WHITE), pygame.Rect(63*i-100,2*86,30,30))
+            pygame.draw.rect(screen, (WHITE), pygame.Rect(63*i-100,3*86,30,30))
+            pygame.draw.rect(screen, (WHITE), pygame.Rect(63*i-100,4*86,30,30))
+            
     # --- Go ahead and refresh the screen with what we've drawn.
     pygame.display.flip()
-    print(count)
-    count = 0
  
     # --- Limit to 60 frames per second
     clock.tick(60)
