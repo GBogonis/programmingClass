5ca461000916a7c74672459c06a7f2e4771be39a
GBogonis <george.bogonis@student39.org>
2023-02-10 09:46:14 -0500

    snow man :3

--- animation.py
+++ animation.py
@@ -1,5 +1,6 @@
 #start
 import random
+import math
 try:
     import pygame
 except:
@@ -12,6 +13,9 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (19,24,98)
+BROWN = (150, 75, 0)
+GREY = (100,100,100)
+MOON_GREY = (48, 70, 96)
  
 pygame.init()
  
@@ -30,9 +34,6 @@ for i in range(100):
     x = random.randrange(0, screenWidth)
     y = random.randrange(0, screenHight)
     snowList.append([x, y])
-
-moonX = 0
-moonY = -.1*moonX^2+350
  
 # Set variable to run loop until the user clicks the close button.
 done = False
@@ -48,16 +49,23 @@ while not done:
             done = True
  
     # --- Game logic should go here
-    moonY = (.007*(moonX**2)+(moonX*.25))+100
+
     # --- Drawing code should go here
     screen.fill(BLUE)
+    #ground
     pygame.draw.rect(screen, (WHITE), pygame.Rect(0, 400, screenWidth, 100))
 
+    #moon
+    pygame.draw.circle(screen, (MOON_GREY), (15,15), 60)
+    pygame.draw.circle(screen, (GREY), (20,40), 15)
+    pygame.draw.circle(screen, (GREY), (50,10), 10)
+
+    #snow flake stuff
     #for each snow flake in the list
     for i in range(len(snowList)):
         #drawing the snow
         pygame.draw.circle(screen, WHITE, snowList[i], 2)
-        # Move the snow flake down 5 pixel
+        #move the snow flake down 5 pixel so it looks like it's falling
         snowList[i][1] += 5
 
         #if the snow flake has moved off the bottom of the screen it gets reset
@@ -68,6 +76,26 @@ while not done:
             #give it a new x position
             x = random.randrange(0, screenWidth)
             snowList[i][0] = x
+    
+    #snowman stuff
+    
+    #stick arms
+    pygame.draw.line(screen, (BROWN), (300,240), (250,150) ,4)
+    pygame.draw.line(screen, (BROWN), (400,240), (450,350) ,4)
+    #snowman body
+    pygame.draw.circle(screen, (WHITE), (350,340), 75)#bottom
+    pygame.draw.circle(screen, (WHITE), (350,240), 55)#middle
+    pygame.draw.circle(screen, (WHITE), (350,160), 35)#top
+    #top hat
+    pygame.draw.rect(screen,(BLACK), pygame.Rect(325,120,50,10))
+    pygame.draw.rect(screen,(BLACK), pygame.Rect(335,90,30,40))
+    #face
+    pygame.draw.circle(screen, (BLACK), (340,150), 7)
+    pygame.draw.circle(screen, (BLACK), (360,150), 7)
+    pygame.draw.arc(screen, (BLACK), [330,145,40,40], math.pi/1, math.pi/18, 3)
+
+    
+
     
     # --- Go ahead and refresh the screen with what we've drawn.
     pygame.display.flip()
