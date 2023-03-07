b5f38640a84ee34dec9140da7cdd3e76b90f650f
GBogonis <george.bogonis@student39.org>
2023-03-07 09:14:49 -0500

    game

---
+++ gameProject/enemy.py
@@ -0,0 +1,33 @@
+try:
+    import pygame.locals
+    import random
+    import game
+except:
+    print("could not import")
+    exit()
+
+
+#animation variables
+enemyList = []
+
+for i in range(10):
+    x = random.randrange(0, game.screenSize)
+    y = random.randrange(0, game.screenHight)
+    enemyList.append([x, y])
+
+def drawEnemy(pos):
+    
+    for i in range(len(enemyList)):
+            #drawing the snow
+            pygame.draw.circle(screen, BLACK, enemyList[i], 2)
+            pygame.draw.circle(game.screen, game.colors["BLACK"], enemyList[i], 2)
+            #move the snow flake down 5 pixel so it looks like it's falling
+
+            if(enemyList[i][0] < playerPosX):
+                enemyList[i][0]  += playerPosX/1000
+            elif(enemyList[i][0] > playerPosX):
+                enemyList[i][0]  -= playerPosX/1000
+            if(enemyList[i][1]  < playerPosY):
+                enemyList[i][1]  += playerPosY/1000
+            elif(enemyList[i][1]  > playerPosY):
+                enemyList[i][1]  -= playerPosY/1000
--- gameProject/game.py
+++ gameProject/game.py
@@ -1,5 +1,7 @@
 try:
     import pygame
+    import random
+    import enemy
 except:
     print("could not import pygame")
     exit() 
@@ -10,14 +12,18 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+colors = {'BLACK':(0,0,0),"WHITE":(255, 255, 255),"GREEN":(0, 255, 0),"RED":(255, 0, 0),"BLUE":(0, 0, 255)}
  
 pygame.init()
  
 # Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
+screenWidth = 700
+screenHight = 700
+screenSize = (screenWidth, screenHight)
+screen = pygame.display.set_mode(screenSize)
  
 pygame.display.set_caption("My Pygame")
+
  
 # Set variable to run loop until the user clicks the close button.
 done = False
@@ -30,28 +36,63 @@ playerPosY = 250
 playerPos = playerPosX, playerPosY
 playerHealth = 250
 
-enemyPosX = 0
-enemyPosY = 0
- 
+
+attacking = False
+'''
+#animation variables
+enemyList = []
+
+
+for i in range(10):
+    x = random.randrange(0, screenWidth)
+    y = random.randrange(0, screenHight)
+    enemyList.append([x, y])
+
+def drawEnemy(pos):
+    pygame.draw.circle(screen, BLACK, pos, 2)
+'''
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event to break loop when user quits
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    
+        if event.type == pygame.MOUSEBUTTONUP:
+            attacking = True
+        
+
     # --- Game logic should go here
-    if(enemyPosX != playerPosX):
-        enemyPosX += playerPosX/100
-    if(enemyPosY != playerPosY):
-        enemyPosY += playerPosY/100
+    
 
     
 
     # --- Drawing code should go here
-    screen.fill(WHITE)
+    screen.fill(colors["WHITE"])
     pygame.draw.circle(screen,(BLUE),(playerPos),50)
-    pygame.draw.circle(screen,(RED),(enemyPosX,enemyPosY),30)
+    if(attacking):
+        target = pygame.mouse.get_pos()
+        currentPos =[playerPos]
+        pygame.draw.circle(screen,(colors["RED"]),(pygame.mouse.get_pos()),10)
+        attacking = False
+
+    '''
+    for i in range(len(enemyList)):
+        #drawing the snow
+        pygame.draw.circle(screen, BLACK, enemyList[i], 2)
+        drawEnemy(enemyList[i])
+        #move the snow flake down 5 pixel so it looks like it's falling
+
+        if(enemyList[i][0] < playerPosX):
+            enemyList[i][0]  += playerPosX/1000
+        elif(enemyList[i][0] > playerPosX):
+            enemyList[i][0]  -= playerPosX/1000
+        if(enemyList[i][1]  < playerPosY):
+            enemyList[i][1]  += playerPosY/1000
+        elif(enemyList[i][1]  > playerPosY):
+            enemyList[i][1]  -= playerPosY/1000
+    '''
+
+    
     # --- Go ahead and refresh the screen with what we've drawn.
     pygame.display.flip()
  
