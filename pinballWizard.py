0991febdd27fd328d1e350d02f81d47f656aca70
GBogonis <george.bogonis@student39.org>
2023-03-24 09:49:23 -0400

    code

--- pinballWizard.py
+++ pinballWizard.py
@@ -26,22 +26,35 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Pygame")
  
 # load ball image 
-ball = pygame.image.load("intro_ball.gif")
+ballSprite = pygame.image.load("intro_ball.gif")
 # frame surface of the rectanglular dimensions of the ball surface
-ballrect = ball.get_rect() 
+
 
 
 #animation variables
 snowList = []
 snowListSpeed = []
 
-for i in range(100):
+for i in range(5):
     x = random.randrange(0, screenWidth)
     y = random.randrange(0, screenHight)
     speed = [5, 5]
     snowList.append([x, y])
     snowListSpeed.append(speed)
  
+#animation variables
+ballList = []
+ballListSpeed = []
+ballPos = []
+ballSpeed = [5, 5]
+
+for i in range(5):
+    ballPos.append([random.randrange(0, screenWidth),random.randrange(0, screenHight)])
+    ballListSpeed.append(ballSpeed)
+    ballrect = ballSprite.get_rect()
+    ballList.append(ballrect)
+    
+    
 
 # Set variable to run loop until the user clicks the close button.
 going = True
@@ -60,19 +73,33 @@ while going:
    
     screen.fill(colors["BLACK"]) # note we refill the screen w/ black every flip() so no ghost trails :)
 
+    '''
     # --- Drawing code should go here
     for i in range(len(snowList)):
         #drawing the snow
-        pygame.draw.circle(screen, WHITE, snowList[i], 2)
+        pygame.draw.circle(screen, colors["BLUE"], snowList[i], 10)
         #screen.blit(ball, snowList[i])
+        
         #move the snow flake down 5 pixel so it looks like it's falling
         snowList[i][0] += snowListSpeed[i][0]
         snowList[i][1] += snowListSpeed[i][1]
 
-        if snowList[i][0] < 0 or snowList[i][0] > screenWidth: # uses edges of ballrect surface to detect collision with screen surface
+        if snowList[i][0] < 11 or snowList[i][0] > screenWidth-11: # uses edges of ballrect surface to detect collision with screen surface
             snowListSpeed[i][0] = -snowListSpeed[i][0] # reverse horizontal speed to stay on screen
-        if snowList[i][1] < 0 or snowList[i][1] > screenHight:
+        if snowList[i][1] < 11 or snowList[i][1] > screenHight-11:
             snowListSpeed[i][1] = -snowListSpeed[i][1] # reverse vertical speed to stay on screen
+    '''
+
+    for i in range(len(ballList)):
+        ballPos[i][0] += ballListSpeed[i][0]
+        ballPos[i][1] += ballListSpeed[i][1]
+        screen.blit(ballSprite,ballPos[i])
+        if ballPos[i][0] < 11 or ballPos[i][0] > screenWidth-11: # uses edges of ballrect surface to detect collision with screen surface
+            ballListSpeed[i][0] = -ballListSpeed[i][0] # reverse horizontal speed to stay on screen
+        if ballPos[i][1] < 11 or ballPos[i][1] > screenHight-11:
+            ballListSpeed[i][1] = -ballListSpeed[i][1] # reverse vertical speed to stay on screen
+
+        
 
     #screen.blit(ball, ballrect) # screen.blit takes 2 params - the first, ball, is drawn onto the second, ballrect surface
     pygame.display.flip()
