2fbcce4266ec462f6bb778d893f7ba02a59282e2
GBogonis <george.bogonis@student39.org>
2023-04-17 09:50:50 -0400

    sadge

--- pinballWizard3.py
+++ pinballWizard3.py
@@ -29,25 +29,19 @@ pygame.display.set_caption("My Pygame")
 #https://pygame.readthedocs.io/en/latest/rect/rect.html
 # load ball image
 ballSprite = pygame.image.load("intro_ball.gif")
-ball1Speed = [5,5]
-ball2Speed = [5,5]
-ball1Rect = ballSprite.get_rect()
-ball2Rect = ballSprite.get_rect()
-ball1Rect.x = random.randrange(0, screenWidth-111)
-ball1Rect.y = random.randrange(0, screenHight-111)
-ball2Rect.x = random.randrange(0, screenWidth-111)
-ball2Rect.y = random.randrange(0, screenHight-111)
-
 ballRectList = []
 ballSpeedList = []
-for i in range(3):
-    ballSpeedList.append([1,1])
+for i in range(4):
+    ballSpeedList.append([5,5])
     ballRect = ballSprite.get_rect()
     ballRect.x = random.randrange(0, screenWidth-111)
     ballRect.y = random.randrange(0, screenHight-111)
     ballRectList.append(ballRect)
-
-
+'''
+class ball():
+    def __init__(self,pos):
+        self.pos = pos
+'''      
 
 
 # Set variable to run loop until the user clicks the close button.
@@ -73,49 +67,40 @@ while going:
         if ballRectList[i].top < 0 or ballRectList[i].bottom > screenHight:
             ballSpeedList[i][1] = -ballSpeedList[i][1] # reverse vertical speed to stay on screen
 
-        balltop = (ballRectList[i].x + 50, ballRectList[i].top)
-        ballleft = (ballRectList[i].left, ballRectList[i].y + 50)
-        ballright = (ballRectList[i].right, ballRectList[i].y + 50)
-        ballbottom = (ballRectList[i].x + 50, ballRectList[i].bottom)
         for e in range(len(ballRectList)):
             if(e != i):
-                balltopE = (ballRectList[e].x + 50, ballRectList[e].top)
-                ballleftE = (ballRectList[e].left, ballRectList[e].y + 50)
-                ballrightE = (ballRectList[e].right, ballRectList[e].y + 50)
-                ballbottomE = (ballRectList[e].x + 50, ballRectList[e].bottom)
                 
-                if ballRectList[i].collidepoint(ballleftE):
+                if ballRectList[i].colliderect(ballleftE):
                     ballSpeedList[e][0] = -ballSpeedList[e][0]
                     print("I collide E: left")
-                if ballRectList[i].collidepoint(balltopE):
+                if ballRectList[i].colliderect(balltopE):
                     ballSpeedList[e][1] = -ballSpeedList[e][1]
                     print("I collide E: top")
-                if ballRectList[i].collidepoint(ballrightE):
+                if ballRectList[i].colliderect(ballrightE):
                     ballSpeedList[e][0] = -ballSpeedList[e][0]
                     print("I collide E: right")
-                if ballRectList[i].collidepoint(ballbottomE):
+                if ballRectList[i].colliderect(ballbottomE):
                     ballSpeedList[e][1] = -ballSpeedList[e][1]
                     print("I collide E: bottom")
                 
-                if ballRectList[e].collidepoint(ballleft):
+                if ballRectList[e].colliderect(ballleft):
                     ballSpeedList[i][0] = -ballSpeedList[i][0]
                     print("E collide I: left")
-                if ballRectList[e].collidepoint(balltop):
+                if ballRectList[e].colliderect(balltop):
                     ballSpeedList[i][1] = -ballSpeedList[i][1]
                     print("E collide I: top")
-                if ballRectList[e].collidepoint(ballright):
+                if ballRectList[e].colliderect(ballright):
                     ballSpeedList[i][0] = -ballSpeedList[i][0]
                     print("E collide I: right")
-                if ballRectList[e].collidepoint(ballbottom):
+                if ballRectList[e].colliderect(ballbottom):
                     ballSpeedList[i][1] = -ballSpeedList[i][1]
                     print("E collide I: bottom")
-            
         screen.blit(ballSprite, ballRectList[i])
  
     pygame.display.flip()
  
     # --- Limit to 60 frames per second
-    clock.tick(100)
+    clock.tick(30)
  
 # Close the window and quit.
 pygame.quit()
