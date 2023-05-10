df11f0c75fc51f69b9c27984f851776aa611d2ed
GBogonis <george.bogonis@student39.org>
2023-05-10 09:49:42 -0400

    end

--- gameProject/game2.py
+++ gameProject/game2.py
@@ -19,7 +19,7 @@ screenSize = (screenWidth, screenHight)
 screen = pygame.display.set_mode(screenSize)
 
  
-pygame.display.set_caption("Surviver")
+pygame.display.set_caption("My Pygame")
 
  
 # Set variable to run loop until the user clicks the close button.
@@ -28,7 +28,7 @@ going = True
 # Used to manage how fast the screen refreshes
 clock = pygame.time.Clock()
 
-#bullet class, it's my own work but I did have help from stackoverflow 
+
 class Bullet:
     def __init__(self, X, Y):
         self.pos = (X, Y)
@@ -60,7 +60,6 @@ class Bullet:
         self.bullet_rect = self.bullet.get_rect(center = self.pos)
         surf.blit(self.bullet, self.bullet_rect)  
 
-#enemy class, this is the thing I'm most proud of tbh
 class Enemy:
     def __init__(self, X, Y, rect):
         #the class is created with the start position as the argument so we don't have to worry about that here
@@ -85,7 +84,6 @@ class Enemy:
         else:
             self.enemydir = (self.enemydir[0]/length, self.enemydir[1]/length)
     
-    #funtion to set speed for later rounds
     def setSpeed(self, newSpeed):
         self.speed = newSpeed
     
@@ -125,7 +123,6 @@ class Enemy:
     def getAlive(self):
         return self.alive
 
-#player class, this is less expansive than I thought it would end up being
 class player:
     #simple player class, tbh not really needed but its fun
     def __init__(self, pos, rect):
@@ -133,9 +130,7 @@ class player:
         self.HP = 100
         self.playerRect = rect
     
-
     def update(self, enemyRectList, enemyAliveList):
-        #hit detection, its difficult to do when everything is in lists
         for i in range(len(enemyRectList)):
             if(enemyAliveList[i] == True and self.playerRect.colliderect(enemyRectList[i])):
                 self.HP -= 1
@@ -150,11 +145,10 @@ bullets = []
 enemyList = []
 attacking = False
 roundNum = 1
-font = pygame.font.Font('freesansbold.ttf', 32)
-#only player object, the 'm' is a naming convention from java that I picked up
 mPlayer = player((screenWidth/2, screenHight/2), Rect(screenWidth/2, screenHight/2, 60, 60))
+font = pygame.font.Font('freesansbold.ttf', 32)
 
-#function for spawning enemies for each round
+
 def spawnEnemies(num):
     #random spawn location code for enemies
     for i in range(num):
@@ -167,27 +161,53 @@ def spawnEnemies(num):
                 ranY = screenHight
         enemyList.append(Enemy(ranX,ranY,Rect(200, 500, 50, 50)))
 
-#inital enemy spawn
 spawnEnemies(5)
-
+movingUp = False
 # -------- Main Program Loop -----------
 while going:
     # --- Main event to break loop when user quits
+    move_ticker = 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             going = False
         if event.type == pygame.MOUSEBUTTONUP:
-            #is the player clicking the mouse to fire a bullet? add it to the list
             bullets.append(Bullet(*mPlayer.playerPos))
-   
+        '''
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                movingUp = True
+        else:
+            movingUp = False
+        '''
+    location = 0
+    keys=pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        if move_ticker == 0:
+            move_ticker = 10
+            location -= 1
+            if location == -1:
+                location = 0
+    if keys[K_RIGHT]:
+        if move_ticker == 0:   
+            move_ticker = 10     
+            location+=1
+            if location == 5:
+                location = 4
+    
+    if(location == 1):
+        print('left')
+    elif(location == -1):
+        print('right')
+
+    if move_ticker > 0:
+        move_ticker -= 1
     #main game code
     screen.fill(colors["WHITE"])
     bulletRectlist = []
     enemyRectList = []
-    #most of the game code will only run when the player is alive
     if(mPlayer.HP > 0):
-        #text for player health and current round
         HPText = font.render('HP:' + str(mPlayer.HP),True, (0, 0, 0))
+
         RoundText = font.render('Round: ' + str(roundNum),True, (0, 0, 0))
         screen.blit(HPText,(10,10))
         screen.blit(RoundText,(10,screenHight-30))
@@ -207,18 +227,18 @@ while going:
             enemyAliveList.append(enemy.getAlive())
             enemyRectList.append(enemy.enemyRect)
 
-        #if all the enemies are dead, start next round
+
+        
         if True not in enemyAliveList:
             roundNum += 1
             spawnEnemies(4+roundNum)
             mPlayer.HP += 50
         
-        #draw and update player
+        
+        #draw player
         mPlayer.update(enemyRectList,enemyAliveList)
         mPlayer.draw(screen)
     else:
-        #if the game is over, display ending text
-
         endText1 = font.render("Game over!", True, (0, 0, 0))
         endRect1 = endText1.get_rect()
         endRect1.center = (screenWidth / 2, screenHight / 2)
@@ -238,5 +258,3 @@ while going:
  
 # Close the window and quit.
 pygame.quit()
-
-#note, I did want to add sounds and a background and other stuff but I was too focused on the game design parts and even so I didn't get all the features that I wanted
