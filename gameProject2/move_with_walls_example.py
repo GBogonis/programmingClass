d1ce949a48894016538179ff166cbed5dadf9a00
GBogonis <george.bogonis@student39.org>
2023-05-24 09:47:55 -0400

    zelda

--- gameProject2/move_with_walls_example.py
+++ gameProject2/move_with_walls_example.py
@@ -1,34 +1,18 @@
-#start
-try:
-    import pygame
-    from pygame.locals import *
-    import random
-    import math
-except:
-    print("could not import pygame")
-    exit() 
-    
-# Define some colors
-colors = {'BLACK':(0,0,0),"WHITE":(255, 255, 255),"GREEN":(0, 255, 0),"RED":(255, 0, 0),"BLUE":(0, 0, 255)}
- 
-pygame.init()
- 
-#256 × 240 pixels
-# Set the width and height of the screen [width, height]
-screenHight = 7*100
-screenWidth = 8*100
-size = (screenWidth, screenHight)
-screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("My Pygame")
+import pygame
  
-# Set variable to run loop until the user clicks the close button.
-going = True
+# -- Global constants
  
-# Used to manage how fast the screen refreshes
-clock = pygame.time.Clock()
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (50, 50, 255)
  
-
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+ 
+ 
 class Player(pygame.sprite.Sprite):
     """ This class represents the bar at the bottom that the player
     controls. """
@@ -40,7 +24,7 @@ class Player(pygame.sprite.Sprite):
  
         # Set height, width
         self.image = pygame.Surface([15, 15])
-        self.image.fill(colors["BLACK"])
+        self.image.fill(WHITE)
  
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
@@ -85,8 +69,8 @@ class Player(pygame.sprite.Sprite):
                 self.rect.bottom = block.rect.top
             else:
                 self.rect.top = block.rect.bottom
-
-
+ 
+ 
 class Wall(pygame.sprite.Sprite):
     """ Wall the player can run into. """
     def __init__(self, x, y, width, height):
@@ -96,54 +80,56 @@ class Wall(pygame.sprite.Sprite):
  
         # Make a blue wall, of the size specified in the parameters
         self.image = pygame.Surface([width, height])
-        self.image.fill(colors["BLUE"])
+        self.image.fill(BLUE)
  
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
  
-
-
+ 
+# Call this function so the Pygame library can initialize itself
+pygame.init()
+ 
+# Create an 800x600 sized screen
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+ 
+# Set the title of the window
+pygame.display.set_caption('Test')
+ 
 # List to hold all the sprites
 all_sprite_list = pygame.sprite.Group()
  
 # Make the walls. (x_pos, y_pos, width, height)
 wall_list = pygame.sprite.Group()
  
-#walls
-
-#left wall
-wall = Wall(0, 0, 50, screenHight)
+wall = Wall(0, 0, 10, 600)
 wall_list.add(wall)
 all_sprite_list.add(wall)
  
-#top wall
-wall = Wall(10, 0, screenWidth, 50)
+wall = Wall(10, 0, 790, 10)
 wall_list.add(wall)
 all_sprite_list.add(wall)
  
-
-wall = Wall(50, 100, 50, 50)
+wall = Wall(10, 200, 100, 10)
 wall_list.add(wall)
 all_sprite_list.add(wall)
  
-
-
-
 # Create the player paddle object
 player = Player(50, 50)
 player.walls = wall_list
-
-all_sprite_list.add(player)
-# -------- Main Program Loop -----------
-going = True
  
-while going:
+all_sprite_list.add(player)
+ 
+clock = pygame.time.Clock()
+ 
+done = False
+ 
+while not done:
  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            going = False
+            done = True
  
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -167,7 +153,7 @@ while going:
  
     all_sprite_list.update()
  
-    screen.fill(colors["WHITE"])
+    screen.fill(BLACK)
  
     all_sprite_list.draw(screen)
  
