676c40d4f2d580ece3a4fbf46714cbe498bde15b
GBogonis <george.bogonis@student39.org>
2023-06-07 11:43:41 -0400

    comments

--- gameProject2/PacMan.py
+++ gameProject2/PacMan.py
@@ -2,12 +2,11 @@
 try:
     import pygame
     from pygame.locals import *
-    import random
-    from math import atan, cos, sin
 except:
     print("could not import pygame")
     exit() 
     
+
 # Define some colors
 colors = {'BLACK':(0,0,0),"WHITE":(255, 255, 255),"GREEN":(0, 255, 0),"RED":(255, 0, 0),"BLUE":(0, 0, 255),"YELLOW":(255, 255, 0)}
  
@@ -99,6 +98,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.x = 10
         if(self.rect.x < 0):
             self.rect.x = screenWidth-10
+
+        #code for eating the dots and powerups
         for dot in dot_hit_list:
             dot_hit_list.remove(dot)
             score += 100
@@ -107,56 +108,34 @@ class Player(pygame.sprite.Sprite):
             score += 1000
 
 
-class Wall(pygame.sprite.Sprite):
-    """ Wall the player can run into. """
-    def __init__(self, x, y):
-        """ Constructor for the wall that the player can run into. """
-        # Call the parent's constructor
+class mapObject(pygame.sprite.Sprite):
+
+    def __init__(self,type, x, y):
+        #call sprite constructor
         super().__init__()
- 
-        # Make a blue wall, of the size specified in the parameters
-        self.image = pygame.Surface([25, 25])
-        self.image.fill(colors["BLUE"])
- 
-        # Make our top-left corner the passed-in location.
+
+        #depending on the type thats passed in, the result is a different map element (wall, dot, powerup)
+        #this was better than having 3 different classes for each
+        if(type == 'w'):
+            #making a simple blue 25 by 25 wall
+            self.image = pygame.Surface([25, 25])
+            self.image.fill(colors["BLUE"])
+        elif(type == 'd'):
+            #making a dot that can be eaten by the player
+            self.image = pygame.Surface([10, 10])
+            self.image.fill(colors["WHITE"])
+        elif(type == 'p'):
+            #making a power up for the player to eat 
+            self.image = pygame.Surface([15, 15])
+            self.image.fill(colors["WHITE"])
+
+        #defining our x and y cords
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
 
-class Dot(pygame.sprite.Sprite):
-    """ Wall the player can run into. """
-    def __init__(self, x, y):
-        """ Constructor for the wall that the player can run into. """
-        # Call the parent's constructor
-        super().__init__()
- 
-        # Make a blue wall, of the size specified in the parameters
-        self.image = pygame.Surface([10, 10])
-        self.image.fill(colors["WHITE"])
- 
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-class Power(pygame.sprite.Sprite):
-    """ Wall the player can run into. """
-    def __init__(self, x, y):
-        """ Constructor for the wall that the player can run into. """
-        # Call the parent's constructor
-        super().__init__()
- 
-        # Make a blue wall, of the size specified in the parameters
-        self.image = pygame.Surface([15, 15])
-        self.image.fill(colors["WHITE"])
- 
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-
-
+#start of ghost class: deprecated
+'''
 class Ghost(pygame.sprite.Sprite):
     def __init__(self,color):
         super.__init__()
@@ -167,11 +146,11 @@ class Ghost(pygame.sprite.Sprite):
         self.rect.y = screenHight/2
         self.rect.x = screenWidth/2
 
-
-# List to hold all the sprites
+'''
+#list to hold every instance of every object, pretty much just to be displayed
 all_sprite_list = pygame.sprite.Group()
  
-# Make the walls. (x_pos, y_pos, width, height)
+#lists for each type of object in the game
 wall_list = pygame.sprite.Group()
 power_list = pygame.sprite.Group()
 dot_list = pygame.sprite.Group()
@@ -179,33 +158,43 @@ dot_list = pygame.sprite.Group()
 
 levelText = open(r'c:\Users\George.Bogonis\Documents\GitHub\programmingClass\gameProject2\level.txt','r')
 
+#level constructor:
+#I have a text file where I lay out the map by using characters (w = wall, d = dot, p = powerup)
+#This section will go though each line and each character in that line a place the corrispondng object in that space
+#this was a solution to the problem of trying to lay out each block of the map by hand, it would have taken for ever,
+# and it would have been unreadable to anyone who tried to understand which block corrisponded to what section of the map
+#this is a elegant solution to a problem that solves every issue I had, and when I got it working I was very proud of myself :)
+
+#vars to keep track
 currentRow = 0
 currentBlock = 0
+#going through each line in the text file
 for row in levelText:
  currentBlock = 0
  currentRow +=1
+ #for each 'block' or character in each line
  for block in row:
     if(block == 'w'):
-        wall = Wall(currentBlock*25,currentRow*25)
-        wall_list.add(wall)
-        all_sprite_list.add(wall)
+        mapElement = mapObject('w',currentBlock*25,currentRow*25)
+        wall_list.add(mapElement)
     if(block == "d"):
-        dot = Dot((currentBlock*25)+7,(currentRow*25)+7)
-        dot_list.add(dot)
-        all_sprite_list.add(dot)
+        mapElement = mapObject('d',(currentBlock*25)+7,(currentRow*25)+7)
+        dot_list.add(mapElement)
     if(block == 'p'):
-        power = Power(currentBlock*25+5,currentRow*25+5)
-        power_list.add(power)
-        all_sprite_list.add(power)
+        mapElement = mapObject('p',currentBlock*25+5,currentRow*25+5)
+        power_list.add(mapElement)
+    all_sprite_list.add(mapElement)
     currentBlock +=1
  
-# Create the player paddle object
+#making the (only) player object and setting the vars 
 player = Player(screenWidth/2, (25*27))
 player.walls = wall_list
 player.dots = dot_list
 player.powers = power_list
 all_sprite_list.add(player)
-font = pygame.font.Font('freesansbold.ttf', 32)
+
+#font for score text 
+textFont = pygame.font.Font('freesansbold.ttf', 32)
 # -------- Main Program Loop -----------
 going = True
  
@@ -214,7 +203,7 @@ while going:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             going = False
- 
+            #movement keys for the player
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 player.changespeed(-5, 0)
@@ -225,14 +214,21 @@ while going:
             if event.key == pygame.K_DOWN:
                 player.changespeed(0, 5)
 
+    #updating all sprites 
     all_sprite_list.update()
  
+    #resetting the screen to avoid trails
     screen.fill(colors["BLACK"])
  
+    #drawing all sprites 
     all_sprite_list.draw(screen)
     
-    scoreText = font.render('Score: ' + str(score),True, (0, 0, 0))
+    #score text setup
+    scoreText = textFont.render('Score: ' + str(score),True, (255, 255, 255))
+    scoreTextRect = scoreText.get_rect()
+    screen.blit(scoreText, (10.10))
 
+    #updating screen stuff
     pygame.display.flip()
  
     clock.tick(30)
