90565c55ce0abd7b84db6e64ec360deaa096903a
GBogonis <george.bogonis@student39.org>
2023-10-25 09:49:54 -0400

    user input half way done

--- WeatherApp/weatherApp.py
+++ WeatherApp/weatherApp.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from jinja2 import Template
+#from textblob import TextBlob
 import random
 import requests
 import json
@@ -13,8 +13,10 @@ apiText = apiCall.text
 apiData = json.loads(apiText)
 
 temp = apiData['main']['temp']
+temp = round(temp)
 description = apiData['weather'][0]['description']
 feelsLike = apiData['main']['feels_like']
+feelsLike = round(feelsLike)
 humidity = apiData['main']['humidity']
 windSpeed = apiData['wind']['speed']
 funnyMessagesList = ["copyright: me :3", 
@@ -29,15 +31,25 @@ funnyMessagesList = ["copyright: me :3",
 funnyMessage = random.choice(funnyMessagesList)
 
 
-@app.route("/")
+@app.route("/userinput")
 
-def weatherApp():
+def userInput():
+    context = {'funnyMessage' : funnyMessage}
+    return render_template('userInput.html', **context)
+
+@app.route("/weatherApp/", methods = ['POST', 'GET'])
+def weatherApp():   
+    
     context = {'temprature' : temp, 
                'description' : description,
                'funnyMessage' : funnyMessage,
                'feelsLike' : feelsLike,
                'humidity' : humidity,
                'windSpeed' : windSpeed}
+    if request.method == 'GET':
+        return render_template('error.html')
+    if request.method == 'POST':
+        
     return render_template('home.html', **context)
 
 @app.route("/about")
