a6199968893ba074cad350006a3cef9e9db98f4e
GBogonis <george.bogonis@student39.org>
2023-11-27 09:49:02 -0500

    historical weather data collection

--- weatherDataCollection.py
+++ weatherDataCollection.py
@@ -2,25 +2,21 @@ import requests
 import json
 
 
-'''
-1668970400
-1637434400
-1605898400
-1574276000
-1574276000
-1511204000
-1479668000
-1448045600
-1416509600
-1384973600
-'''
 
-apiCall = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=42.82642299955682&lon=-71.57923160207767&units=imperial&appid=15dfa1324b44123c1634401424dd71a1')
+
+#apiCall = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=42.82642299955682&lon=-71.57923160207767&units=imperial&appid=15dfa1324b44123c1634401424dd71a1')
 #apiCall = requests.get('https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=42.82642299955682&lon=-71.57923160207767&dt=1668970400&appid=15dfa1324b44123c1634401424dd71a1')
-print('code:', apiCall.status_code)
-apiJson = apiCall.text
-data = json.loads(apiJson)
+year = 2022
+tempList = []
+while year >= 2000: 
+    print('start')
+    yearStr = str(year)
+    apiCall = requests.get('https://archive-api.open-meteo.com/v1/archive?latitude=42.826287240545135&longitude=-71.5792990891693&start_date=' + yearStr +'-11-27&end_date=' + yearStr +'-11-27&daily=temperature_2m_mean&temperature_unit=fahrenheit&wind_speed_unit=ms&precipitation_unit=inch')
+    apiJson = apiCall.text
+    data = json.loads(apiJson)
+    tempList.append(data['daily']['temperature_2m_mean'])
+    year -= 1
+    print('end')
 
-print(data['name'])
+print(tempList)
 
-print(data)
