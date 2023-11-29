f5ee280523b093a9d4b2b3b3b5df090b1b3f9a49
GBogonis <george.bogonis@student39.org>
2023-11-29 09:48:14 -0500

    ML start

--- MLtestScript.py
+++ MLtestScript.py
@@ -1,2 +1,10 @@
-import sklearn
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
+data1 = np.array([34.1, 32.2, 40.0, 31.8, 36.6, 45.9, 35.8, 35.2, 18.3, 25.9, 32.9, 53.0, 34.5, 42.7, 35.1, 37.2, 43.2, 48.2, 41.7, 47.9, 24.1, 39.3, 37.8]).reshape((-1,1))
+data2 = np.array([34.1, 32.2, 40.0, 31.8, 36.6, 45.9, 35.8, 35.2, 18.3, 25.9, 32.9, 53.0, 34.5, 42.7, 35.1, 37.2, 43.2, 48.2, 41.7, 47.9, 24.1, 39.3, 37.8])
+
+model = LinearRegression()
+model.fit(data1,data2)
+pred = model.predict(data1)
+print(pred)
--- weatherDataCollection.py
+++ weatherDataCollection.py
@@ -7,16 +7,19 @@ import json
 #apiCall = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=42.82642299955682&lon=-71.57923160207767&units=imperial&appid=15dfa1324b44123c1634401424dd71a1')
 #apiCall = requests.get('https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=42.82642299955682&lon=-71.57923160207767&dt=1668970400&appid=15dfa1324b44123c1634401424dd71a1')
 year = 2022
+day = 29
+month = 11
 tempList = []
-while year >= 2000: 
-    print('start')
-    yearStr = str(year)
-    apiCall = requests.get('https://archive-api.open-meteo.com/v1/archive?latitude=42.826287240545135&longitude=-71.5792990891693&start_date=' + yearStr +'-11-27&end_date=' + yearStr +'-11-27&daily=temperature_2m_mean&temperature_unit=fahrenheit&wind_speed_unit=ms&precipitation_unit=inch')
+while year >= 1982: 
+    print(year)
+    #print('start')
+    apiCall = requests.get('https://archive-api.open-meteo.com/v1/archive?latitude=42.826287240545135&longitude=-71.5792990891693&start_date=' + str(year) +'-'+str(month)+'-'+str(day)+'&end_date=' + str(year) +'-'+str(month)+'-'+str(day)+'&daily=temperature_2m_mean&temperature_unit=fahrenheit&wind_speed_unit=ms&precipitation_unit=inch')
     apiJson = apiCall.text
     data = json.loads(apiJson)
-    tempList.append(data['daily']['temperature_2m_mean'])
+    tempList.append(data['daily']['temperature_2m_mean'][0])
     year -= 1
-    print('end')
+    #print('end')
 
 print(tempList)
+
 
