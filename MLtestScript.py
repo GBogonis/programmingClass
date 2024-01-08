48a6156a539024f064852d154995adc153b1e738
GBogonis <george.bogonis@student39.org>
2024-01-08 11:18:28 -0500

    ML stuff

--- MLtestScript.py
+++ MLtestScript.py
@@ -1,7 +1,7 @@
 import numpy as np
-from sklearn.preprocessing import Imputer
+#from sklearn.preprocessing import Imputer
 import pandas as pd
-
+import tensorflow as tf
 '''
 data1 = np.array([34.1, 32.2, 40.0, 31.8, 36.6, 45.9, 35.8, 35.2, 18.3, 25.9, 32.9, 53.0, 34.5, 42.7, 35.1, 37.2, 43.2, 48.2, 41.7, 47.9, 24.1, 39.3, 39.8]).reshape((-1,1))
 data2 = np.array([34.1, 32.2, 40.0, 31.8, 36.6, 45.9, 35.8, 35.2, 18.3, 25.9, 32.9, 53.0, 34.5, 42.7, 35.1, 37.2, 43.2, 48.2, 41.7, 47.9, 24.1, 39.3, 37.8])
@@ -14,3 +14,29 @@ print(pred)
 file = open('weatherData2.txt','r')
 dataFrame = pd.read_csv(file)
 print(dataFrame.shape)
+
+val_dataframe = dataFrame.sample(frac=0.2, random_state=1337)
+train_dataframe = dataFrame.drop(val_dataframe.index)
+
+print(
+    "Using %d samples for training and %d for validation"
+    % (len(train_dataframe), len(val_dataframe))
+)
+
+def dataframe_to_dataset(dataframe):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop("target")
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    ds = ds.shuffle(buffer_size=len(dataframe))
+    return ds
+
+
+train_ds = dataframe_to_dataset(train_dataframe)
+val_ds = dataframe_to_dataset(val_dataframe)
+
+for x, y in train_ds.take(8):
+    print("Input:", x)
+    print("Target:", y)
+    
+train_ds = train_ds.batch(32)
+val_ds = val_ds.batch(32)
--- weatherDataCollection.py
+++ weatherDataCollection.py
@@ -1,30 +1,41 @@
 import requests
 import json
 
-output = open("weatherData2.txt","w")
+output = open("weatherData3.txt","w")
 
 
 #apiCall = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=42.82642299955682&lon=-71.57923160207767&units=imperial&appid=15dfa1324b44123c1634401424dd71a1')
 #apiCall = requests.get('https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=42.82642299955682&lon=-71.57923160207767&dt=1668970400&appid=15dfa1324b44123c1634401424dd71a1')
 year = 1982
 
-startYear = 1982
-endYear= 2022
-day = 20
-month = 12
-tempList = []
+#startYear = 1982
+endYear= 2023
+day = '01'
+month = '01'
+#dataList = []
+
+#apiCall = requests.get('https://archive-api.open-meteo.com/v1/archive?latitude=42.826287240545135&longitude=-71.5792990891693&start_date=' + str(year) +'-'+str(month)+'-'+str(day)+'&end_date=' + str(year) +'-'+str(month)+'-'+str(day)+'&daily=temperature_2m_mean&temperature_unit=fahrenheit&wind_speed_unit=ms&precipitation_unit=inch')
+'''
+apiCall = requests.get("https://archive-api.open-meteo.com/v1/archive?latitude=42.82642299955682&longitude=-71.57923160207767&start_date=2023-01-02&end_date=2023-01-02&daily=temperature_2m_mean,sunshine_duration,precipitation_sum,wind_speed_10m_max,shortwave_radiation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto")
+apiJson = apiCall.text
+data = json.loads(apiJson)
+print(data)
+'''
+#print(data['daily']['temperature_2m_mean'],data['daily']['sunshine_duration'],data['daily']['precipitation_sum'],data['daily']['wind_speed_10m_max'],data['daily']['shortwave_radiation_sum'])
+startLine = "month,"+"temp,"+"sunshine,"+"precipitation,"+"windspeed,"+"radiation"+"\n"
+#output.write(startLine)
+
 while year <= endYear: 
     print(year)
     
     #print('start')
-    apiCall = requests.get('https://archive-api.open-meteo.com/v1/archive?latitude=42.826287240545135&longitude=-71.5792990891693&start_date=' + str(year) +'-'+str(month)+'-'+str(day)+'&end_date=' + str(year) +'-'+str(month)+'-'+str(day)+'&daily=temperature_2m_mean&temperature_unit=fahrenheit&wind_speed_unit=ms&precipitation_unit=inch')
+    apiCall = requests.get('https://archive-api.open-meteo.com/v1/archive?latitude=42.82642299955682&longitude=-71.57923160207767&start_date=' + str(year) + '-' + month + '-' + day + '&end_date=' + str(year) + '-' + month + '-' + day + '&daily=temperature_2m_mean,sunshine_duration,precipitation_sum,wind_speed_10m_max,shortwave_radiation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto')
     apiJson = apiCall.text
     data = json.loads(apiJson)
-    tempList.append(data['daily']['temperature_2m_mean'][0])
-    outputStr = (str(year) + "," + str(data['daily']['temperature_2m_mean'][0]) +"\n")
+    #dataList.append(data['daily']['temperature_2m_mean'][0],data['daily']['sunshine_duration'][0],data['daily']['precipitation_sum'][0],data['daily']['wind_speed_10m_max'][0],data['daily']['shortwave_radiation_sum'][0])
+    outputStr = (month + ',' + str(data['daily']['temperature_2m_mean'][0]) + ',' + str(data['daily']['sunshine_duration'][0]) + ',' + str(data['daily']['precipitation_sum'][0]) + ',' + str(data['daily']['wind_speed_10m_max'][0]) + ',' + str(data['daily']['shortwave_radiation_sum'][0]) +"\n")
     print(outputStr)
     output.write(outputStr)
     
     year += 1
     #print('end')
-
