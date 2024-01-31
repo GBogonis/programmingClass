1fbad22439b81e19e80048387faebc476ccc5554
GBogonis <george.bogonis@student39.org>
2024-01-31 09:37:01 -0500

    robot stuff

---
+++ teamDataCollector.py
@@ -0,0 +1,19 @@
+import statbotics
+
+mStatbotics = statbotics.Statbotics()
+
+numWin = 0
+numLost = 0
+numTie = 0
+numTotal = 0
+
+for i in range(2023,2001,-1):
+    print(i)
+    if(i != 2021):
+        data = mStatbotics.get_team_year(138,i)
+        numWin += data['full_wins']
+        numLost += data['full_losses']
+        numTie += data['full_ties']
+        numTotal += data['full_count']
+
+print(numWin/numTotal)
