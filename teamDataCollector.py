2c704052df09a8307044e46ab6421f5381b1ee5b
GBogonis <george.bogonis@student39.org>
2024-02-06 11:03:30 -0500

    this takes forever

--- teamDataCollector.py
+++ teamDataCollector.py
@@ -2,18 +2,62 @@ import statbotics
 
 mStatbotics = statbotics.Statbotics()
 
-numWin = 0
-numLost = 0
-numTie = 0
-numTotal = 0
+class team:
+    def __init__(self, teamNum):
+        self.number = teamNum
+        self.wins = 0
+        self.lost = 0
+        self.tie = 0
+        self.total = 0
+        
+    def getTeamNum(self):
+        return self.number
+    
+    def addWin(self,gamesWon):
+        self.wins += gamesWon
+        
+    def addLost(self,gamesLost):
+        self.lost += gamesLost
+        
+    def addTie(self,gamesTie):
+        self.tie += gamesTie
+        
+    def addTotal(self,gamesTotal):
+        self.total += gamesTotal
+        
+    def getWinRate(self):
+        return (self.wins/(self.total-self.tie))
+    
+    def getRookieYear(self):
+        try:
+            return mStatbotics.get_team(int(self.number))['rookie_year']
+        except:
+            return "no rookie year"
+        
+        
+
+teamsFile = open("teams.txt")
+teams = []
+for i in teamsFile:
+    teams.append(team(i))
+
 
 for i in range(2023,2001,-1):
-    print(i)
+    
     if(i != 2021):
-        data = mStatbotics.get_team_year(138,i)
-        numWin += data['full_wins']
-        numLost += data['full_losses']
-        numTie += data['full_ties']
-        numTotal += data['full_count']
+        for t in teams:  
+            try:
+                print(i)
+                print(t.number)
+                if(int(t.getRookieYear()) < i):
+                    data = mStatbotics.get_team_year(int(t.number),i)
+                    t.addWin(data['full_wins'])
+                    t.addLost(data['full_losses']) 
+                    t.addTie(data['full_ties'])
+                    t.addTotal(data['full_count'])
+            except:
+                print("no data")
+                continue
 
-print(numWin/numTotal)
+for i in teams:
+    print(i.number, " has a winrate of ", i.getWinRate())
