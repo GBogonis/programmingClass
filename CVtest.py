303af2a2f0b38de860f39778faddfb540b7d300e
GBogonis <george.bogonis@student39.org>
2024-05-01 09:38:42 -0400

    cv stuffs

---
+++ CVtest.py
@@ -0,0 +1,223 @@
+#THIS CODE IS NOT MINE, I USED CTRL+C AND CTRL+V A LOT
+#THIS WAS TO LEARN AND UNDERSTAND WHY THING DO OR DON'T WORK
+
+#https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python#video
+#https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/index#models
+
+
+
+
+'''
+# Importing Libraries 
+import cv2 
+import mediapipe as mp 
+
+# Used to convert protobuf message to a dictionary. 
+from google.protobuf.json_format import MessageToDict 
+
+# Initializing the Model 
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands(static_image_mode=False,model_complexity=1,min_detection_confidence=0.75,min_tracking_confidence=0.75, max_num_hands=2) 
+
+# Start capturing video from webcam 
+cap = cv2.VideoCapture(0) 
+
+while True: 
+	# Read video frame by frame 
+	success, img = cap.read() 
+
+	# Flip the image(frame) 
+	img = cv2.flip(img, 1) 
+
+	# Convert BGR image to RGB image 
+	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+
+	# Process the RGB image 
+	results = hands.process(imgRGB) 
+
+	# If hands are present in image(frame) 
+	if results.multi_hand_landmarks: 
+
+		# Both Hands are present in image(frame) 
+		if len(results.multi_handedness) == 2: 
+				# Display 'Both Hands' on the image 
+			cv2.putText(img, 'Both Hands', (250, 50), 
+						cv2.FONT_HERSHEY_COMPLEX, 
+						0.9, (0, 255, 0), 2) 
+
+		# If any hand present 
+		else: 
+			for i in results.multi_handedness: 
+				
+				# Return whether it is Right or Left Hand 
+				label = MessageToDict(i)['classification'][0]['label']
+				if label == 'Left': 
+					
+					# Display 'Left Hand' on 
+					# left side of window 
+					cv2.putText(img, label+' Hand', 
+								(20, 50), 
+								cv2.FONT_HERSHEY_COMPLEX, 
+								0.9, (0, 255, 0), 2) 
+
+				if label == 'Right': 
+					
+					# Display 'Left Hand' 
+					# on left side of window 
+					cv2.putText(img, label+' Hand', (460, 50), 
+								cv2.FONT_HERSHEY_COMPLEX, 
+								0.9, (0, 255, 0), 2) 
+
+	# Display Video and when 'q' 
+	# is entered, destroy the window 
+	cv2.imshow('Image', img) 
+	if cv2.waitKey(1) & 0xff == ord('q'): 
+		break
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import numpy as np
+import cv2 
+import time
+
+model_path = '/absolute/path/to/hand_landmarker.task'
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+
+
+
+
+# Load the frame rate of the video using OpenCV’s CV_CAP_PROP_FPS
+# You’ll need it to calculate the timestamp for each frame.
+
+#img.getfield(cv2.CAP_PROP_FPS)
+
+
+# Loop through each frame in the video using VideoCapture#read()
+
+
+# Convert the frame received from OpenCV to a MediaPipe’s Image object.
+#mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
+
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+	print('hand landmarker result: {}'.format(result))
+
+options = HandLandmarkerOptions(base_options=BaseOptions(model_asset_path='absolute/path/to/hand_landmarker.task'),running_mode=VisionRunningMode.LIVE_STREAM,result_callback=print_result)
+
+with HandLandmarker.create_from_options(options) as landmarker:
+	cap = cv2.VideoCapture(0)
+    
+	while True:
+		# Read video frame by frame 
+		success, img = cap.read() 
+		if not success:
+			break
+
+		cv2.imshow("img", img)
+		#frame = cap.get(cv2.CAP_PROP_FPS)
+		timestamp = int(round(time.time() * 1000))
+
+		frame_np = np.array(img)
+		mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_np)
+		landmarker.detect_async(mp_image, timestamp)
+'''
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import cv2
+import time
+import numpy as np
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+RESULT = None
+
+
+# Create a hand landmarker instance with the live stream mode:
+def print_result(result: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    # print(result)
+    global RESULT
+    RESULT = result
+
+
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='absolute/path/to/hand_landmarker.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+with HandLandmarker.create_from_options(options) as landmarker:
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_np = np.array(frame)
+        timestamp = int(round(time.time() * 1000))
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_np)
+        frame = mp_image.numpy_view()
+        landmarker.detect_async(mp_image, timestamp)
+        if type(RESULT) is not type(None):
+            hand_landmarks_list = RESULT.hand_landmarks
+            for idx in range(len(hand_landmarks_list)):
+                hand_landmarks = hand_landmarks_list[idx]
+
+                # Draw the hand landmarks.
+                hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+                hand_landmarks_proto.landmark.extend([
+                    landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in
+                    hand_landmarks
+                ])
+                solutions.drawing_utils.draw_landmarks(
+                    frame,
+                    hand_landmarks_proto,
+                    solutions.hands.HAND_CONNECTIONS,
+                    solutions.drawing_styles.get_default_hand_landmarks_style(),
+                    solutions.drawing_styles.get_default_hand_connections_style())
+        else:
+            print('else')
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+'''
+import mediapipe as mp
+
+BaseOptions = mp.tasks.BaseOptions
+FaceDetector = mp.tasks.vision.FaceDetector
+FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a face detector instance with the live stream mode:
+def print_result(result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int):
+    print('face detector result: {}'.format(result))
+
+options = FaceDetectorOptions(
+    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+with FaceDetector.create_from_options(options) as detector:
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_frame_from_opencv)
+    detector.detect_async(mp_image, frame_timestamp_ms)
+'''
--- teamDataCollector.py
+++ teamDataCollector.py
@@ -9,60 +9,28 @@ class team:
         self.lost = 0
         self.tie = 0
         self.total = 0
-        
-    def getTeamNum(self):
-        return self.number
-    
-    def addWin(self,gamesWon):
-        self.wins += gamesWon
-        
-    def addLost(self,gamesLost):
-        self.lost += gamesLost
-        
-    def addTie(self,gamesTie):
-        self.tie += gamesTie
-        
-    def addTotal(self,gamesTotal):
-        self.total += gamesTotal
-        
+        self.rookieYear = mStatbotics.get_team(int(self.number))['rookie_year']
+      
     def getWinRate(self):
         if(self.total == 0):
             return 0
         return (self.wins/(self.total-self.tie))
     
-    def getRookieYear(self):
-        try:
-            return mStatbotics.get_team(int(self.number))['rookie_year']
-        except:
-            return "no rookie year"
+    def setData(self):
+        for i in range(2023,2001,-1): 
+            if(i != 2021):
+                if(int(self.rookieYear) <= i):
+                    data = mStatbotics.get_team_year(int(self.number),i)
+                    self.wins += data["full_wins"]
+                    self.lost += data["full_losses"]
+                    self.tie += data["full_ties"]
+                    self.total += data["full_count"]
 
 
 
 teamsFile = open("teams.txt")
 teams = []
 
-
 for i in teamsFile:
     teams.append(team(i))
 
-
-for i in range(2023,2001,-1):
-    
-    if(i != 2021):
-        for t in teams:  
-            try:
-                print(i)
-                print(t.number)
-                if(int(t.getRookieYear()) < i):
-                    data = mStatbotics.get_team_year(int(t.number),i)
-                    t.addWin(data['full_wins'])
-                    t.addLost(data['full_losses']) 
-                    t.addTie(data['full_ties'])
-                    t.addTotal(data['full_count'])
-            except:
-                print("no data")
-                continue
-
-for i in teams:
-    
-    print(str(i.number) + " has a winrate of " + str(i.getWinRate()))
