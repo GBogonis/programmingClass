f3556581a980a6db08fdda83cb5749c7d15f1186
GBogonis <95968706+GBogonis@users.noreply.github.com>
2024-06-04 17:16:24 +0100

    first finished build

--- Game.py
+++ Game.py
@@ -1,13 +1,13 @@
 import random
 #start
 
-armors = {"none":0, "leather":5, "bronze":10, "iron":15, "steel":20, "titanium":25}
-swords = {"none":0, "wood":-10, "bronze":-20, "iron":-40, "steel":-60, "titanium":-100}
+armors = {"none":0, "leather":5, "bronze":10, "iron":15, "titanium":20}
+swords = {"none":0, "wood":-10, "bronze":-20, "iron":-40, "titanium":-60}
 magics = {"fireball": -20, "healing prayer": 30, "great fireball": -50, "great healing preyer": 60}
 #(damage, number of turns)
 status = {"poison": (-5,5), "fire": (-10,3)}
 #enemy values
-enemyDamage = {"slime":10, "goblin": 30, "orc": 40}
+enemyDamage = {"slime": -10, "goblin": -20, "orc": -30}
 enemyHealth = {"slime":20, "goblin": 40, "orc": 50}
 
 cavelayer = 10
@@ -41,19 +41,25 @@ class enemy:
 #handles turns and interactions
 class controller:
     def __init__(self):
-        self.turn = 1
+        self.turns = 1
 
-    def turn(self, playerDPS, playerTarget):
-        print("you attacked a " + str(enemyList[playerTarget].type) + " for " + str(playerDPS) + " damage!")
-        enemyList[playerTarget].health += playerDPS
+    def turn(self, playerDPS, playerTarget, playerResist):
+        print("you attacked a " + str(enemyList[playerTarget-1].type) + " for " + str(playerDPS) + " damage!")
+        enemyList[playerTarget-1].health += playerDPS
+        if(enemyList[playerTarget-1].health > 0):
+            print("The ", enemyList[playerTarget-1].type, " is left with ", enemyList[playerTarget-1].health, " HP!")
+        else:
+            print("you have slain the ", enemyList[playerTarget-1].type)
+
         for i in enemyList:
             if(i.health > 0):
-                mPlayer.health += i.damage
+
+                mPlayer.health += (i.damage + playerResist)
                 print("you were hit by a " + str(i.type) + " for " + str(i.damage) + " damage!")
             else:
                 enemyList.remove(i)
         print("you are now at " + str(mPlayer.health) + " HP")
-        self.turn += 1
+        self.turns += 1
 
 enemyList = []
 mPlayer = player()
@@ -97,9 +103,56 @@ while cavelayer != 0:
 
     while(len(enemyList) != 0):
         playerMove = mPlayer.turn(enemyList)
-        print("player move: ", playerMove)
-        mController.turn(playerMove)
+        mController.turn(playerMove[0],playerMove[1], armors[mPlayer.armor])
     
-    cavelayer += 1
+    floorMessage = random.randint(1,5)
+    if(floorMessage == 1):
+        print("floor complete! you might make it out of this alive")
+    elif(floorMessage == 2):
+        print("floor complete! you start to feel like a true hero")
+    elif(floorMessage == 3):
+        print("floor complete! if you knew magic this would be a lot easier")
+    elif(floorMessage == 4):
+        print("floor complete! you are suprised at your own capability")
+    elif(floorMessage == 5):
+        print("floor complete! if you make it out of here, you wonder who you will tell about this")
+
+    if(cavelayer != 1):
+        print("you rest for a bit and scavange the battlefield")
+        mPlayer.health = 100
+        lootSword = random.randint(1,10)
+        if(lootSword > cavelayer+5):
+            print("You find a titanium sword! it's heavy but you can deal")
+            mPlayer.sword = "titanium"
+        elif(lootSword > cavelayer+3 and mPlayer.dps > -60):
+            print("You find a iron sword! it's lighter than you expect but the handle has seen better days")
+            mPlayer.sword = "iron"
+        elif(lootSword > cavelayer and mPlayer.dps > -40):
+            print("You find a bronze sword! part of the blade is chiped but it cuts fine")
+            mPlayer.sword = "bronze"
+        elif(mPlayer.dps > -10):
+            print("You find a wood sword! it isn't great but it better than nothing")
+            mPlayer.sword = "wood"
+        else:
+            print("You couldn't find a better weapon")
+
+        lootArmor = random.randint(1,10)
+        if(lootArmor > cavelayer+5):
+            print("You find titanium armor! you might as well be a real knight at this point")
+            mPlayer.armor = "titanium"
+        elif(lootArmor > cavelayer+3 and armors[mPlayer.armor] < 15):
+            print("You find iron armor! you pretend you are a knight to feel better about the situation")
+            mPlayer.armor = "iron"
+        elif(lootArmor > cavelayer and armors[mPlayer.armor] < 10):
+            print("You find bronze armor! you wonder if the Ancient Greeks were the last ones here")
+            mPlayer.armor = "bronze"
+        elif(mPlayer.dps > -10):
+            print("You find leather armor! you don't know what animal's leather but that doesn't matter now")
+            mPlayer.armor = "leather"
+        else:
+            print("You couldn't find any better protection")
+    cavelayer -= 1
 
 print("you have escaped the cave, congratulations!")
+print("thank you for playing")
+exit()
