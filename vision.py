95ba1947684078841c426883687607e67b12f64e
GBogonis <95968706+GBogonis@users.noreply.github.com>
2024-06-07 14:00:51 +0100

    pi back up stuff

--- vision.py
+++ vision.py
@@ -4,6 +4,13 @@
 import cv2
 import time
 import mediapipe as mp
+import time
+import numpy as np
+from imutils.video import VideoStream
+import imutils
+
+usingPiCamera = True
+frameSize = (320,240)
 
 # Grabbing the Holistic Model from Mediapipe and
 # Initializing the Model
@@ -16,19 +23,21 @@ holistic_model = mp_holistic.Holistic(
 # Initializing the drawing utils for drawing the facial landmarks on image
 mp_drawing = mp.solutions.drawing_utils
 # (0) in VideoCapture is used to connect to your computer's default camera
-capture = cv2.VideoCapture(0)
-
+# capture = cv2.VideoCapture(0)
+vs = VideoStream(src = 0, usePiCamera=usingPiCamera, resolution=frameSize,framerate=32).start()
+time.sleep(2.0)
+timeCheck = time.time()
 # Initializing current time and precious time for calcu lating the FPS
 previousTime = 0
 currentTime = 0
 
-while capture.isOpened():
+while True:
 	# capture frame by frame
-	ret, frame = capture.read()
+	frame = vs.read()
 
 	# resizing the frame for better view
-	frame = cv2.resize(frame, (800, 600))
-
+	#frame = cv2.resize(frame, (800, 600))
+	 
 	# Converting the from BGR to RGB
 	image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -41,7 +50,7 @@ while capture.isOpened():
 
 	# Converting back the RGB image to BGR
 	image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
+	
 	# Drawing Right hand Land Marks
 	mp_drawing.draw_landmarks(
 	image, 
